ca50be03687c3f181d43d72fa59363b5f7a38917
James Pham <jamespham93@yahoo.com>
2020-04-08 10:47:02 -0700

    Add hook to override WC templates

--- wp-content/themes/twentytwenty-child/functions.php
+++ wp-content/themes/twentytwenty-child/functions.php
@@ -29,6 +29,14 @@ function theme_start_session()
         session_start();
 }
 
+/**
+ * OVERRIDE WooCommerce Templates
+ */
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+function mytheme_add_woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+
 /** Woo Commerce Hooks */
 
 /**
