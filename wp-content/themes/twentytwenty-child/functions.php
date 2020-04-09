405164e288dfd11dea3ef3db0aaa4d0fd5108223
James Pham <jamespham93@yahoo.com>
2020-04-09 14:38:18 -0700

    Fix entry_id to show in cart, checkout, order item

--- wp-content/themes/twentytwenty-child/functions.php
+++ wp-content/themes/twentytwenty-child/functions.php
@@ -31,16 +31,48 @@ function theme_start_session()
 
 /** WooCommerce Hooks */
 
+/** USER FRONT-END CHANGES */
+/**
+ * REMOVE related products output
+ *
+ * This hook removes the related products on each product page (Standard-Size Printing, Large Format Printing)
+ * @see - https://docs.woocommerce.com/document/remove-related-posts-output/
+ */
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
+
+/**
+ * REDIRECT "Continue Shopping" to "services" page
+ */
+add_filter( 'woocommerce_continue_shopping_redirect', 'wc_custom_redirect_continue_shopping' );
+function wc_custom_redirect_continue_shopping()
+{
+    //return your desired link here.
+    return get_site_url() . '/services';
+}
+
+/**
+ * CHANGE "Add to Cart" button text to "Add Order"
+ */
+add_filter( 'woocommerce_product_add_to_cart_text', 'woocommerce_custom_product_add_to_cart_text' );
+function woocommerce_custom_product_add_to_cart_text()
+{
+    return __( 'Add Order', 'woocommerce' );
+}
+
 /**
  * OVERRIDE WooCommerce Templates
  */
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
-function mytheme_add_woocommerce_support() {
+function mytheme_add_woocommerce_support()
+{
     add_theme_support( 'woocommerce' );
 }
 
 /**
  * REMOVE Shipping and Billing Fields in Checkout
+ *
+ * @param $fields - associative array of checkout fields
+ * @return mixed - updated fields with billing section stripped
  */
 add_filter( 'woocommerce_checkout_fields', 'custom_override_checkout_fields' );
 function custom_override_checkout_fields( $fields )
@@ -64,68 +96,88 @@ function custom_override_checkout_fields( $fields )
     return $fields;
 }
 
-/**
- * REDIRECT "Continue Shopping" to "services" page
- */
-add_filter( 'woocommerce_continue_shopping_redirect', 'wc_custom_redirect_continue_shopping' );
-function wc_custom_redirect_continue_shopping()
-{
-    //return your desired link here.
-    return get_site_url() . '/services';
-}
-
+/** BACK-END, METADATA, & WP ADMIN CHANGES  */
 
 /**
- * CHANGE "Add to Cart" button text to "Add Order"
- */
-add_filter( 'woocommerce_product_add_to_cart_text', 'woocommerce_custom_product_add_to_cart_text' );
-function woocommerce_custom_product_add_to_cart_text()
-{
-    return __( 'Add Order', 'woocommerce' );
-}
-
-
-/// HOOKS TO ATTACH ENTRY_ID TO ORDER META DATA
-/**
- * PROGRAMMATICALLY add entry_id to Product meta data. GF entry_id should be accessible from SESSIONS (set in plugin)
- */
-add_action( 'woocommerce_add_to_cart', 'add_to_cart_with_entry_id', 10, 6 );
-function add_to_cart_with_entry_id( $cart_item_key, $product_id, $quantity, $variation_id, $variation, $cart_item_data )
-{
-
-}
-
-/**
- * ADD the order meta with entry_id
- */
-add_action( 'woocommerce_checkout_update_order_meta', 'entry_id_update_order_meta' );
-function entry_id_update_order_meta( $order_id )
-{
-    session_start();
-    $entry_id = $_SESSION['entry_id'];
-    update_post_meta( $order_id, 'bc_entry_id', $entry_id );
-}
-
-/**
- * Display field value on the order edit page with a link to download
- * @TODO - will need to revise in the future. This is for business cards specifically...
- */
-add_action( 'woocommerce_admin_order_data_after_billing_address', 'my_custom_checkout_field_display_admin_order_meta', 10, 1 );
-function my_custom_checkout_field_display_admin_order_meta( $order )
-{
-    $entry_id = get_post_meta( $order->get_id(), 'bc_entry_id', true );
-    $uploads = wp_upload_dir();
-
-    echo "<strong style='color: black;'>" . __( 'Entry ID' ) . ":</strong>
-          <a href='" . esc_url( $uploads['baseurl'] . '/business_cards/business_card_' . $entry_id . '.pdf') . "'> 
-            <span> Business Card $entry_id </span>
-          </a>";
-}
-
-/**
- * REMOVE related products output
+ * ADD business card entry_id to cart item.
  *
- * This hook removes the related products on each product page (Standard-Size Printing, Large Format Printing)
- * @see - https://docs.woocommerce.com/document/remove-related-posts-output/
+ * @param array $cart_item_data
+ * @param int $product_id
+ * @param int $variation_id
+ *
+ * @return array
  */
-remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
+add_filter( 'woocommerce_add_cart_item_data', 'add_entry_id_to_cart_item', 10, 3 );
+function add_entry_id_to_cart_item( $cart_item_data, $product_id, $variation_id )
+{
+    $entry_id = $_SESSION['entry_id'];
+
+    if ( empty( $entry_id ) ) {
+        return $cart_item_data;
+    }
+
+    if ( $product_id === 39 ) {
+        $cart_item_data['bc_entry_id'] = $entry_id;
+    }
+
+    return $cart_item_data;
+}
+
+
+/**
+ * DISPLAY first + last name text of business card order in the cart.
+ *
+ * @param array $item_data
+ * @param array $cart_item
+ *
+ * @return array
+ */
+add_filter( 'woocommerce_get_item_data', 'business_card_display_text_cart', 10, 2 );
+function business_card_display_text_cart( $item_data, $cart_item )
+{
+    if ( empty( $cart_item['bc_entry_id'] ) ) {
+        return $item_data;
+    }
+
+    $item_data[] = array(
+        'key' => __( 'Business Card Entry ID', 'bc_entry_id' ),
+        'value' => wc_clean( $cart_item['bc_entry_id'] ),
+        'display' => '',
+    );
+
+    return $item_data;
+}
+
+
+/**
+ * ADD business card PDF link to
+ *
+ * @param WC_Order_Item_Product $item
+ * @param string $cart_item_key
+ * @param array $values
+ * @param WC_Order $order
+ */
+function bc_entry_id_text_to_order_items( $item, $cart_item_key, $values, $order )
+{
+    if ( empty( $values['bc_entry_id'] ) ) {
+        return;
+    }
+
+    // get upload directory + entry_id
+    $uploads = wp_upload_dir();
+    $entry_id = $values['bc_entry_id'];
+
+    // pull name from business_card
+    $entry = GFAPI::get_entry($entry_id);
+    $firstname = $entry['2.3'];
+    $lastname = $entry['2.6'];
+
+    //
+    $item->add_meta_data( __( 'Business Card Entry ID', 'bc_entry_id' ),
+        "
+            <a href='" . esc_url( $uploads['baseurl'] . '/business_cards/business_card_' . $entry_id . '.pdf' ) . "'>
+             " . "$firstname" . " " . "$lastname" . "
+            </a>
+        "
+    );
+}
