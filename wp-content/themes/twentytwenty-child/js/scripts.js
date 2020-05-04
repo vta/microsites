166fadf443c6ee230d7638c0735cd76704e8a43d
James Pham <jamespham93@yahoo.com>
2020-05-04 16:47:11 -0700

    Made tab event listener dynamic to include SSP

--- wp-content/themes/twentytwenty-child/js/scripts.js
+++ wp-content/themes/twentytwenty-child/js/scripts.js
@@ -5,10 +5,10 @@
  * @param $ - jQuery selector
  * @param page_num - page number to change to
  */
-const bc_select_tab = ($, page_num) => {
+const add_tab_select = ($, form_id, page_num) => {
 
-  $('#gform_target_page_number_4').val(page_num);
-  $('#gform_4').trigger('submit', [true]);
+  $(`#gform_target_page_number_${form_id}`).val(page_num);
+  $(`#gform_${form_id}`).trigger('submit', [true]);
 
 }
 
@@ -18,16 +18,13 @@ const bc_select_tab = ($, page_num) => {
  * creates event listeners for each tab click
  * @param $ - jQuery Selector
  */
-const bc_tabs = ($) => {
+const bind_tabs_event = ($, form_id) => {
 
-  // BC Tab 1: Your Information
-  $('#gf_step_4_1').live('click', () => bc_select_tab($, '1'));
+  // Loop through the 3 tabs and set up event listeners
+  for (let page_num = 1; page_num <= 3; page_num++) {
+    $(`#gf_step_${form_id}_${page_num}`).live('click', () => add_tab_select($, form_id, page_num));
+  }
 
-  // BC Tab 2: Contact
-  $('#gf_step_4_2').live('click', () => bc_select_tab($, '2'));
-
-  // BC Tab 3: Request
-  $('#gf_step_4_3').live('click', () => bc_select_tab($, '3'));
 }
 
 // STANDARD-SIZE PRINTING FORM
@@ -38,10 +35,10 @@ const bc_tabs = ($) => {
  * runs functions to set up business card form
  * @param $
  */
-const bc_form_setup = ($) => {
+const bc_form_setup = ($, form_id) => {
 
   // add tab functionality to business cards
-  bc_tabs($);
+  bind_tabs_event($, form_id);
 
 }
 
@@ -111,9 +108,18 @@ const ssp_staple_conditional_checkbox = ($, form_id) => {
  */
 const ssp_form_setup = ($, form_id, current_page) => {
 
-  // adding additional classes to SSP form
-  ssp_form_class($, form_id);
-  ssp_staple_conditional_checkbox($, form_id);
+  if (form_id === 1) {
+    // adding additional classes to SSP form
+    ssp_form_class($, form_id);
+
+    // Conditional values for stapling options
+    ssp_staple_conditional_checkbox($, form_id);
+
+    // Add tab functionality to SSP
+    bind_tabs_event($, form_id);
+
+  }
+
 }
 
 /**
@@ -125,7 +131,7 @@ jQuery(document).on('gform_post_render', (event, form_id, current_page) => {
   const $ = jQuery;
 
   // Set up business card form
-  bc_form_setup($);
+  bc_form_setup($, form_id);
 
   // Set up standard-size printing form
   ssp_form_setup($, form_id, current_page);
